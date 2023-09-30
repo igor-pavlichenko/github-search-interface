cbfe1dae96963a37f9ff7a9d0fc64314798e4aa6
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2023-09-30 12:34:56 +0100

    Adjust responsiveness & add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 
-import { Autocomplete, CssBaseline, CssVarsProvider, Sheet, Stack } from '@mui/joy';
+import { Autocomplete, CssBaseline, CssVarsProvider, GlobalStyles, Sheet, Stack } from '@mui/joy';
+import { RxMagnifyingGlass } from 'react-icons/rx';
 
 import { ThemeModeToggle } from './core/ThemeModeToggle';
 
@@ -8,13 +9,37 @@ function App() {
   return (
     <CssBaseline>
       <CssVarsProvider defaultMode="system">
-        <Stack direction="column" gap={1} py={1} px={1}>
-          <ThemeModeToggle />
+        <GlobalStyles
+          styles={{
+            // We recommend using a class over a tag if possible.
+            '.RadixIcon': {
+              color: 'var(--Icon-color)',
+              margin: 'var(--Icon-margin)',
+              fontSize: 'var(--Icon-fontSize, 20px)',
+              width: '1em',
+              height: '1em',
+            },
+          }}
+        />
 
-          <Sheet variant="plain" sx={{ width: '100%', height: '100%' }}>
-            <Autocomplete options={[]} />
-          </Sheet>
-        </Stack>
+        <Sheet variant="plain" sx={{ width: '100%', height: '100%', p: 2 }}>
+          <Stack
+            direction="column"
+            alignItems="center"
+            gap={4}
+            py={{ xs: 1, sm: 2, md: 3 }}
+            px={{ xs: 1, sm: 2, md: 4 }}
+          >
+            <ThemeModeToggle />
+
+            <Autocomplete
+              size="lg"
+              sx={{ width: { xs: '100%', sm: '600px' } }}
+              startDecorator={<RxMagnifyingGlass className="RadixIcon" />}
+              options={[]}
+            />
+          </Stack>
+        </Sheet>
       </CssVarsProvider>
     </CssBaseline>
   );
--- src/core/ThemeModeToggle.tsx
+++ src/core/ThemeModeToggle.tsx
@@ -1,15 +1,21 @@
-import Button from '@mui/joy/Button';
+import { Stack, Switch } from '@mui/joy';
 import { useColorScheme } from '@mui/joy/styles';
+import { RxMoon, RxSun } from 'react-icons/rx';
 
 export const ThemeModeToggle = () => {
   const { mode, setMode } = useColorScheme();
   return (
-    <Button
-      variant="outlined"
-      color="neutral"
-      onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
-    >
-      {mode === 'dark' ? 'Turn light' : 'Turn dark'}
-    </Button>
+    <Stack direction="row" spacing={1} useFlexGap alignItems="center">
+      <RxSun className="RadixIcon" />
+      <Switch
+        sx={{ width: 'fit-content' }}
+        size="lg"
+        variant="outlined"
+        color="neutral"
+        checked={mode === 'dark'}
+        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+      />
+      <RxMoon className="RadixIcon" />
+    </Stack>
   );
 };
