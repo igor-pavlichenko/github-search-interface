8d5865de0f4bc997e934d2fab106eb55aebcc47f
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2023-10-01 02:09:47 +0100

    Highlight favorite repo

--- src/core/components/RepositoryCard/RepositoryCard.tsx
+++ src/core/components/RepositoryCard/RepositoryCard.tsx
@@ -1,5 +1,7 @@
 import { Chip, Sheet, Stack } from '@mui/joy';
 
+import { useFavoriteRepos } from '~/core/state/favoriteRepos';
+
 import { Repository } from '../../api/graphql/searchRepositoriesQuery';
 import Footer from './Footer';
 import Heading from './Heading';
@@ -8,10 +10,20 @@ type Props = { repo: Repository };
 
 const RepositoryCard = ({ repo }: Props) => {
   const { repositoryTopics } = repo;
+  const favoriteEntry = useFavoriteRepos((state) =>
+    state.favoriteRepos.find((fav) => fav.id === repo.id),
+  );
+  const isFavorite = !!favoriteEntry;
   return (
     <Sheet
       variant="outlined"
-      sx={{ width: { xs: '100%', md: '700px' }, borderRadius: 5, padding: 2 }}
+      sx={{
+        width: { xs: '100%', md: '700px' },
+        borderRadius: 5,
+        backgroundColor: ({ palette }) => (isFavorite ? palette.background.level1 : undefined),
+        borderColor: ({ palette }) => (isFavorite ? palette.text.primary : undefined),
+        padding: 2,
+      }}
     >
       <Stack gap="0.5rem" justifyContent="flex-start">
         <Heading repo={repo} />
