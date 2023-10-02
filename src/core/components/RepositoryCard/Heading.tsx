45078b3edde3d2870481d02929cb413f5fd29347
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2023-10-02 18:32:15 +0100

    Improve responsiveness

--- src/core/components/RepositoryCard/Heading.tsx
+++ src/core/components/RepositoryCard/Heading.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Link, Stack, Typography } from '@mui/joy';
+import { Button, Grid, Link, Typography } from '@mui/joy';
 import { RxStar, RxStarFilled } from 'react-icons/rx';
 
 import { Repository } from '~/core/api/graphql/searchRepositoriesQuery';
@@ -18,8 +18,8 @@ const Heading = ({ repo }: Props) => {
 
   return (
     <Grid container justifyContent="space-between" wrap="nowrap">
-      <Stack gap="0.2rem">
-        <Grid container gap="0.2rem" alignItems="center">
+      <Grid xs={8} sm={10} container direction="column" gap="0.2rem">
+        <Grid container gap="0.2rem" alignItems="center" wrap="nowrap">
           <img width={20} height={20} style={{ borderRadius: 5 }} src={owner.avatarUrl} />
           <Link>
             <Typography fontWeight={600}>{`${owner.login}/${name}`}</Typography>
@@ -28,9 +28,9 @@ const Heading = ({ repo }: Props) => {
         <Typography fontSize="0.9rem" textAlign="start">
           {description}
         </Typography>
-      </Stack>
+      </Grid>
 
-      <Grid xs={3} container justifyContent="flex-end" alignItems="baseline">
+      <Grid xs="auto" container justifyContent="flex-end" alignItems="baseline">
         <Button
           color="neutral"
           variant="outlined"
